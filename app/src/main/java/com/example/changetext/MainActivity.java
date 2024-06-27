aeb9d85d14f75824b772a5ca9b2bd3d74caaf983
Gokul-Da <gokul.tn.ind@gmail.com>
2024-06-27 10:48:40 +0530

    Inital commit
    Change text on button click

---
+++ app/src/main/java/com/example/changetext/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.changetext;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+        button.setOnClickListener(v -> textView.setText("Button Clicked!"));
+    }
+}
